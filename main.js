d84de101581b1fa86eaba4b8208affad69ad31ac
dvwedding <44728149+dvwedding@users.noreply.github.com>
2018-11-04 23:55:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,17 +4,23 @@ $(document).ready(function () {
      myFunction();
 	 
 	 $("#linkAbt").click(function(){
+		 $(".overlayDiv").hide();
 		 $("#overlayDivAbout").toggle();
 	 });
 	 $("#linkVenue").click(function(){
+		 $(".overlayDiv").hide();
 		 $("#overlayDivVenue").toggle();
 	 });
 	 $("#linkEI").click(function(){
+		 $(".overlayDiv").hide();
 		 $("#overlayDivEi").toggle();
 	 });
 	 $("#linkImgs").click(function(){
+		 $(".overlayDiv").hide();
 		 $("#overlayDivImgs").toggle();
 	 });
+	 
+	 includeHTML();
 });
 
 
@@ -53,3 +59,31 @@ var x = setInterval(function() {
         document.getElementById("demo").innerHTML = "Engaged !!!";
     }
 }, 1000);
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
