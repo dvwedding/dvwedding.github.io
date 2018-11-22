8c480fc55955e13e2f1ea752dce845d5113c3dbd
dvwedding <44728149+dvwedding@users.noreply.github.com>
2018-11-23 00:16:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -81,14 +81,14 @@ $('html body').keydown(function(e) {
 	 
 });
 function flipCard() {
-    setInterval(flip1, 5000);
+    setInterval(flip1, 10000);
 	
 }
 function flip1(){ 
 $(".flip-card .flip-card-inner").css({
 	"transform" : "rotateX(180deg)"
 });
-setInterval(flip2, 9000);
+setInterval(flip2, 18000);
 }
 
 function flip2(){ 
