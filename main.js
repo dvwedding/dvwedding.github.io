493c5dae8e308156634f2f46103b8ee4c3b69857
dvwedding <44728149+dvwedding@users.noreply.github.com>
2019-05-26 05:54:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ var countDownDate = new Date("May 26, 1989 10:03:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
     
-    var distance =countDownDate - now;
+    var distance =now - countDownDate;
     
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
